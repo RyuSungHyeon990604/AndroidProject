6e736ec2f8a580bf2bdbbf9341fe4a92cf2d73d4
skvhffpdyd <115456416+skvhffpdyd@users.noreply.github.com>
2022-11-18 17:39:09 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,40 +1,38 @@
 package com.example.androidproject
 
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
 import com.example.androidproject.databinding.ActivityMainBinding
-import com.google.firebase.auth.ktx.auth
+
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var firebaseAuth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val login_button=binding.loginButton
-        val sign_up_button=binding.signUpButton
-        login_button.setOnClickListener {
-            val id=binding.editTextTextPersonName.text.toString()//findViewById<EditText>(R.id.editTextTextPersonName)
-            val password=binding.editTextTextPassword.text.toString()//findViewById<EditText>(R.id.editTextTextPassword)
-            Firebase.auth.signInWithEmailAndPassword(id,password)
-                .addOnCompleteListener {
-                    if(it.isSuccessful) {
-                       //메인화면으로 이동
-                    }
-                    else {
-
-//
-                    }
-                }
+        firebaseAuth = FirebaseAuth.getInstance();
+        binding.LoginButton.setOnClickListener {
+            loginEmail()
         }
-        sign_up_button.setOnClickListener {
-            val intent=Intent(this,SignUpActivity::class.java)
-            startActivity(intent)
+        binding.RegisterButton.setOnClickListener {
+
+        }
+    }
+    private fun loginEmail(){
+        firebaseAuth!!.signInWithEmailAndPassword(binding.UserIdText.text.toString()
+            , binding.UserPasswordText.text.toString()).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
