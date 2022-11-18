649dd33302651695c465f412de2a4db2f129ad9c
Sunghyeon Ryu <1871166@hansung.ac.kr>
2022-11-18 17:21:36 +0900

    login and signup activity

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.example.androidproject
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.example.androidproject.databinding.ActivityMainBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //good
-        //hellohello!
-       
+        val binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val login_button=binding.loginButton
+        val sign_up_button=binding.signUpButton
+        login_button.setOnClickListener {
+            val id=binding.editTextTextPersonName.text.toString()//findViewById<EditText>(R.id.editTextTextPersonName)
+            val password=binding.editTextTextPassword.text.toString()//findViewById<EditText>(R.id.editTextTextPassword)
+            Firebase.auth.signInWithEmailAndPassword(id,password)
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                       //메인화면으로 이동
+                    }
+                    else {
+
+//
+                    }
+                }
+        }
+        sign_up_button.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/androidproject/SignUpActivity.kt
+++ app/src/main/java/com/example/androidproject/SignUpActivity.kt
@@ -1,1 +1,34 @@
+package com.example.androidproject
 
+import androidx.appcompat.app.AppCompatActivity
+
+
+import android.os.Bundle
+import com.example.androidproject.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding=ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val signupbtn = binding.signUpButton//findViewById<Button>(R.id.sign_up_button)
+        signupbtn.setOnClickListener {
+
+            signUp("a@a.com","123456")
+        }
+    }
+    private fun signUp(userEmail: String, password: String) {
+        Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    println("성공")
+                } else {
+                    println("실패")
+                }
+            }
+    }
+}
