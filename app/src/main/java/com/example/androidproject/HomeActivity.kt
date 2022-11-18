053b77c81f620750d80c2d2df2ba9a987b922249
h <ysm9770@gmail.com>
2022-11-18 21:04:31 +0900

    커밋메시지

--- app/src/main/java/com/example/androidproject/HomeActivity.kt
+++ app/src/main/java/com/example/androidproject/HomeActivity.kt
@@ -2,9 +2,6 @@ package com.example.androidproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import com.example.androidproject.databinding.ActivityHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -13,6 +10,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         var navBar=binding.navigationView as BottomNavigationView
         supportFragmentManager.beginTransaction().add(R.id.navigationView, HomeFragment()).commit()
@@ -34,11 +32,5 @@ class HomeActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
-
     }
-    fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit()
-    }
-
 }
